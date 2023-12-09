e91ff106f00beac0cf368adbd100d98d7d70bb9b
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-12-09 17:35:26 +0300

    Add prompt for missing query strings

--- posts.js
+++ posts.js
@@ -2,13 +2,27 @@ const postsRow = document.querySelector("#postsRow");
 
 async function getPosts() {
   const urlParams = new URLSearchParams(window.location.search);
-  const myParam = urlParams.get("userId");
-  console.log(myParam);
+console.log(window.location.href);
+
+
+let myParam = "";
+
+if (urlParams.has("userId")) {
+  myParam = urlParams.get("userId");
+  if (myParam === "") {
+    const getUserId2 = prompt("Lütfen User ID giriniz, 1-10 arasında.");
+    myParam = getUserId2;
+  } 
+} else {
+  const getUserId = prompt("Lütfen User ID giriniz, 1-10 arasında.");
+  myParam = getUserId;
+};
 
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?userId=${myParam}`
   );
   data = await response.json();
+
 
   function showPosts() {
     let arr = "";
